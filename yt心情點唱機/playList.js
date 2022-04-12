18d46e75a81f8273199d8f2107f6c95d6bf3ea68
p98113@gmail.com <p98113@gmail.com>
2022-04-13 05:17:39 +0800

    First commit

---
+++ yt心情點唱機/playList.js
@@ -0,0 +1,12 @@
+//YouTube Video ID
+let playList = [
+    "tAWPPcPaiQo",
+    "QI37w-6va-4",
+    "6D79CYTxvOM",
+    ];
+    //播放起訖秒數
+    let playTime = [
+    [78,90],
+    [150,163],
+    [153,172]
+    ];
